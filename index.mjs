7b03985bc5cac03b5dc58bcfb418cd4bd91000ab
ginapedraza <ginapedraza00@gmail.com>
2023-09-23 17:56:23 -0300

    construyendo mdLinks y transformando ruta, aun sin funcionar

--- index.mjs
+++ index.mjs
@@ -1,15 +1,30 @@
 import fs from 'fs/promises';
 import path from 'path';
+import mdLinks from './mdLinks.mjs'
 
 //module.exports = () => {
   // ...
 //};
 
-const mdLinks = (path) => {
-  return new Promise((resolve, reject) => {
-    const absolutePath = path.resolve(path);
-    // Resolver la Promesa con la ruta absoluta.
-    resolve(absolutePath);
-  
-});
+//Transformamos la ruta relativa a absoluta (si ya fuera absoluta, la devuelve igual)
+const transformToAbsolutePath = (receivedPath) => {
+  // Verificar si la ruta recibida es absoluta
+  if (path.isAbsolute(receivedPath)) {
+    return receivedPath; // Si la ruta es absoluta, retornarla sin cambios
+  } else {
+    // Si la ruta es relativa, convertirla a una ruta absoluta
+    return path.resolve(process.cwd(), receivedPath);
+  }
 };
+
+//probando la promesa
+mdLinks(receivedPath)
+.then((absolutePath) => {
+  console.log('Ruta absoluta:', absolutePath);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+
+
+export { transformToAbsolutePath };
