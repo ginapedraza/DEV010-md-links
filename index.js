6d51b856267c876ee1d8e51d6d331d3e6dec5763
ginapedraza <ginapedraza00@gmail.com>
2023-09-25 18:54:41 -0300

    transforma ruta a absoluta, verifica si ruta existe, verifica si es markdown

---
+++ index.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+const { transformToAbsolutePath, checkIfPathExists, checkPathExtension } =  require('./pathAnalysis');
+
+//module.exports = () => {
+  // ...
+//};
+
+//Recibimos la ruta ingresada
+const receivedPath = process.argv[2];
+
+// Construimos la promesa
+const mdLinks = (receivedPath) => {
+  return new Promise((resolve, reject) => {
+    const absolutePath = transformToAbsolutePath(receivedPath);
+    // Resolver la Promesa con la ruta absoluta.
+    resolve(absolutePath);
+   console.log('Ruta absoluta:', absolutePath);
+
+// Verifica si la ruta existe
+    const pathExists = checkIfPathExists(absolutePath);
+    if(!pathExists) {
+      return Promise.reject(new Error('La ruta no existe'));
+    
+    }
+
+// Verifica si es un archivo markdown
+  const fileExtension = checkPathExtension(absolutePath);
+  if(!fileExtension) {
+    return Promise.reject(new Error('El archivo no es markdown'));
+    
+  }
+ 
+
+});
+};
+
+//probando la promesa (esto se debe probar en otro lugar, mdLinks se debe exportar)
+mdLinks(receivedPath)
+.then((absolutePath) => {
+  //console.log('Ruta absoluta:', absolutePath);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+
+
+module.exports = {
+  mdLinks,
+ };
+
+
+    /*let mdFiles = [];
+    const stats = fs.statSync(absolutePath);
+
+    if (stats.isDirectory()) {
+      // Si la ruta es un directorio, leer los archivos .md dentro de él
+      mdFiles = readDir(absolutePath);
+      console.log('es un directorio:', mdFiles);
+    } else if (stats.isFile() && path.extname(absolutePath) === ".md") {
+      // Si la ruta es un archivo .md, agregarlo al array de archivos .md
+      mdFiles = [absolutePath];
+      console.log('es un archivo:',mdFiles);
+    } else {
+      // Si la ruta no es un directorio ni un archivo .md válido, rechazar la promesa con un mensaje de error
+      reject("La ruta especificada no es un directorio ni un archivo .md válido.");
+      return;
+    }*/
+
+
+    //Comprobando si el archivo existe, si es file o directory
+/*fs.stat(absolutePath, (err, stats) => {
+  if( !err ){
+       if(stats.isFile()){
+           console.log('is file ? ' + stats.isFile());
+       }
+ 
+       else if(stats.isDirectory()){
+           console.log('is directory? ' + stats.isDirectory());
+       }
+   }
+   else
+       throw err; 
+});*/
---
+++ pathAnalysis.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path =  require('path');
+
+const files = fs.readdirSync('./');
+console.log(files);
+
+//Transformamos la ruta relativa a absoluta (si ya fuera absoluta, la devuelve igual)
+const transformToAbsolutePath = (receivedPath) => {
+  // Verificar si la ruta recibida es absoluta
+  if (path.isAbsolute(receivedPath)) {
+    console.log('la ruta es absoluta');
+    return receivedPath; // Si la ruta es absoluta, retornarla sin cambios
+  } else {
+    // Si la ruta es relativa, convertirla a una ruta absoluta
+    console.log('Transformamos a ruta absoluta');
+    const absolutePath = path.resolve(process.cwd(), receivedPath);
+    return absolutePath;
+  }
+};
+
+//Verificamos que la ruta exista en la computadora
+const checkIfPathExists = (receivedPath) => {
+  const pathExists = fs.existsSync(receivedPath);
+  console.log(pathExists);
+  return pathExists;
+
+};
+
+//Verificamos que sea un archivo markdown
+const checkPathExtension = (receivedPath) => {
+  const extension = path.extname(receivedPath);
+  const validExtensions = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
+  const pathIsMarkdown = validExtensions.includes(extension);
+  console.log(pathIsMarkdown);
+  return pathIsMarkdown;
+   // console.log('El archivo es markdown');
+  //} else {
+   // console.log('El archivo no es markdown');
+
+  };
+
+
+module.exports = {
+    transformToAbsolutePath,
+    checkIfPathExists,
+    checkPathExtension,
+   };
+  
+
